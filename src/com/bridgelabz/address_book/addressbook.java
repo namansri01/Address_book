9a247404653f9c69f8655933fc814329c2d3b7bc
namansri01 <sri.naman.vastava@gmail.com>
2024-02-20 11:54:02 +0530

    [Naman] Added: Welcome to Address book

--- src/com/bridgelabz/address_book/addressbook.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.address_book;
-
-public class addressbook {
-
-    public static void main(String args[]){
-        System.out.println("Welcome to address bokk programing");
-    }
-}
